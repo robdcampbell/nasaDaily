ee9118332584a8682584c67bed8268211b532d5e
robdcampbell <rdeancampbell@gmail.com>
2021-01-18 11:38:02 -0500

    set up .env file for apikey and attach in NasaPhoto.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Test Homie</h1>
+      <Link to="/nasaphoto">Nasa: Photo of the day.</Link>
     </div>
   );
 };
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+const apiKey = process.env.REACT_APP_NASA_KEY;
 
 const NasaPhoto = () => {
+  const [photoData, setPhotoData] = useState(null);
+
+  const fetchPhoto = async () => {
+    const response = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+    );
+    const data = await response.json();
+    setPhotoData(data);
+  };
+
+  useEffect(() => {
+    fetchPhoto();
+  }, []);
+
+  if (!photoData) return <h1>No photo data retreived!</h1>;
+
   return (
     <div>
-      <h1>This be my Nasa Photo!</h1>
+      {photoData.media_type === "image" ? (
+        <img
+          src={photoData.url}
+          alt={`NASA photo of the day: ${photoData.title}`}
+        />
+      ) : (
+        <iframe
+          title="Nasa-video"
+          src={photoData.url}
+          alt={`NASA photo of the day: ${photoData.title}`}
+          frameBorder="0"
+          gesture="media"
+          allow="excrypted-media"
+          allowFullScreen
+          className="photo"
+        />
+      )}
+
+      <div>
+        <h1>{photoData.title}</h1>
+        <p>{photoData.date}</p>
+        <p>{photoData.explanation}</p>
+      </div>
     </div>
   );
 };
